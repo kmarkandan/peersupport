969372ce866a9ed0602364c7496ff621da2fafe2
kartikmarkandan <kartikmarkandan@shaw.ca>
2012-08-15 22:13:33 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peersupport::Application.config.secret_token = '610d230d10f4f5d9b7feef5117f1fda18b62a6512a8b8cf9449ffa54a4e76c379b9f6ae7f8e5c0bf8a7cdc7b8813939edea4be50483d0e4bd3c388435097cf83'
